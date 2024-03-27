f69049cbf6c47ca2832d21862c4f3605d5dd3d65
Hawaou12 <123965623+Hawaou12@users.noreply.github.com>
2024-03-27 15:59:29 +0100

    modification NotFound

--- Bah_Hawaou_8_18092023/src/components/notFound/NotFound.js
+++ Bah_Hawaou_8_18092023/src/components/notFound/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NotFound.scss";
-import notFound from "../../assets/images/logo404.png";
+
 
 const NotFound = () => {
     return (
